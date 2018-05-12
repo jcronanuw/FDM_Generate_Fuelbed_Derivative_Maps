6c1112eb2b0638322b4b022bed5c085a8cf5e37b
jcronanuw <jcronan@uw.edu>
2018-05-11 19:05:34 -0700

    Adding to analysis. Analysis is close to being able to create maps of different fuelbed metrics.

--- FDM_Output_Analysis.R
+++ FDM_Output_Analysis.R
@@ -27,13 +27,64 @@ setwd("C:/Users/jcronan/Documents/GitHub/FDM-Eglin-Analysis/inputs")
 params <- read.csv("input_params.csv", header=TRUE, 
   sep=",", na.strings="NA", dec=".", strip.white=TRUE)
 
-setwd("C:/usfs_cronan_gis/SEF/FDM_outputs_vFP/run_102")
+fft <- read.csv("fft_outputs.csv", header=TRUE, 
+                sep=",", na.strings="NA", dec=".", strip.white=TRUE)
+
+
+setwd("C:/usfs_sef_outputs_FDM/results_r888")
 
 #Import a single raster file to use header data to reference number of columns for matrix(scan())
-f.head <- raster("r1021101.asc")
+f.head <- raster("f088810005.asc")
 
 #Import .asc files
-fm.1 <- matrix(scan("r1021101.asc",skip = f.head@file@offset),ncol=f.head@ncols,byrow=T)
+f05 <- matrix(scan("f088810005.asc",skip = f.head@file@offset),ncol=f.head@ncols,byrow=T)
+f10 <- matrix(scan("f088810010.asc",skip = f.head@file@offset),ncol=f.head@ncols,byrow=T)
+f15 <- matrix(scan("f088810015.asc",skip = f.head@file@offset),ncol=f.head@ncols,byrow=T)
+f20 <- matrix(scan("f088810020.asc",skip = f.head@file@offset),ncol=f.head@ncols,byrow=T)
+f25 <- matrix(scan("f088810025.asc",skip = f.head@file@offset),ncol=f.head@ncols,byrow=T)
+f30 <- matrix(scan("f088810030.asc",skip = f.head@file@offset),ncol=f.head@ncols,byrow=T)
+f35 <- matrix(scan("f088810035.asc",skip = f.head@file@offset),ncol=f.head@ncols,byrow=T)
+f40 <- matrix(scan("f088810040.asc",skip = f.head@file@offset),ncol=f.head@ncols,byrow=T)
+f45 <- matrix(scan("f088810045.asc",skip = f.head@file@offset),ncol=f.head@ncols,byrow=T)
+f50 <- matrix(scan("f088810050.asc",skip = f.head@file@offset),ncol=f.head@ncols,byrow=T)
+
+#Make a map of litter loading
+f05_litter <- lapply(f05, function(x) fft$LLM_litter_load[match(x, fft$Fuelbed_number)])
+f05_benchmark_ros <- lapply(f05, function(x) fft$Benchmark_ROS[match(x, fft$Fuelbed_number)])
+
+f05_lm <- matrix(f05_litter, f.head@nrows, f.head@ncols)
+str(f05_lm)
+head(f05_lm)
+head(f05_litter)
+length(f05_litter)
+length(f05)
+
+test <- vector()
+for(i in 1:length(f05))
+{
+  test[i] <- length(f05_litter[[i]])
+  }
+
+
+names(fft)
+
+str(f05)
+
+aa <- matrix(c(1,9,1,2,3,4,4,4,1,3,2,1,3,4,4,4),4,4)
+bb <- data.frame(a = c(1,2,3,4), b = c(11, 22, 33, 44))
+
+cc <-lapply(aa, function(x) bb$b[match(x, bb$a)])
+matrix(cc, 4,4)
+
+
+aa[aa == bb$a] <- bb$b
+
+
+bb$a[1]
+
+
+cc <- aa == bb$a]
+cc
 
 
 
---
+++ inputs/readme_fft_outputs.txt
@@ -0,0 +1,5 @@
+FFT Outputs copied from the GitHub FCCS-FirePotentialComparison repository on May 11, 2018
+Original filepath and name: C:\Users\jcronan\Documents\GitHub\FCCS-FirePotentialComparison\inputs\fccs_ros_moistureScenario_7.csv
+This table are the FFT fire behavior outputs used to parameterize probability of igntion values for FDM v 2.0.
+The method used to come up with these fuel moisture scenarios is described here.
+C:\Users\jcronan\Documents\Projects_Incomplete_Files\2012_Fuelbed_Characterization_and_Mapping\Probability_of_Ignition\20171222_PIG_Methods_Cronan.docx
